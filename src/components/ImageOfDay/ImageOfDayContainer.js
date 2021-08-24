9b326ddeac252245566dcc1abfca67f0f41a32e6
Manuel Entrena <manuel.entrena@gmail.com>
2021-08-24 23:12:00 +0200

    Added styles for zoom

--- src/components/ImageOfDay/ImageOfDayContainer.js
+++ src/components/ImageOfDay/ImageOfDayContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useEventsOfDay, useGlobal } from 'hooks';
 import { Spinner, Modal, ImageOfDay } from 'components';
 
@@ -6,6 +6,9 @@ export default function ImageOfDayContainer() {
   const { noScroll } = useGlobal();
   const { imageOfDay: img } = useEventsOfDay();
   const [showModal, setShowModal] = useState(false);
+
+  const imgRef = useRef(null);
+  const containerRef = useRef(null);
 
   useEffect(() => {
     noScroll(showModal);
@@ -19,9 +22,30 @@ export default function ImageOfDayContainer() {
     setShowModal(false);
   };
 
+  const zoom = (e) => {
+    const realImg = imgRef.current;
+    const realWidth = realImg.naturalWidth;
+    const realHeight = realImg.naturalHeight;
+    const ratio = realHeight / realWidth;
+    const percentage = `${ratio * 100}%`;
+    const boxWidth = containerRef.current.clientWidth;
+    const xPos = e.pageX;
+    const yPos = e.pageY;
+    const xPercent = `${xPos / (boxWidth / 100)}%`;
+    const yPercent = `${yPos / ((boxWidth * ratio) / 100)}%`;
+    /* realImg.style.backgroundPosition = `${xPercent} ${yPercent}`;
+    realImg.style.backgroundSize = `${realWidth}px`; */
+    console.log(imgRef);
+    console.log({
+      percentage, boxWidth, xPos, yPos, xPercent, yPercent,
+    });
+  };
+
   const modal = showModal && (
   <Modal onClose={handleOpenClose}>
-    <img className="modal__img" src={img.fullImg} alt={img.alt} />
+    <div ref={containerRef} className="modal__picture">
+      <img ref={imgRef} className="modal__img" src={img.fullImg} alt={img.alt} onMouseMove={(e) => zoom(e)} />
+    </div>
   </Modal>
   );
 
