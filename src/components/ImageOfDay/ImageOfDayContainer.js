2ff1d2002423c0b3331702cbaf13cf35d4235821
Manuel Entrena <manuel.entrena@gmail.com>
2021-08-25 15:19:19 +0200

    Added library LightBox

--- src/components/ImageOfDay/ImageOfDayContainer.js
+++ src/components/ImageOfDay/ImageOfDayContainer.js
@@ -1,14 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useEventsOfDay, useGlobal } from 'hooks';
-import { Spinner, Modal, ImageOfDay } from 'components';
+import { Spinner, ImageOfDay } from 'components';
+import Lightbox from 'react-image-lightbox';
 
 export default function ImageOfDayContainer() {
   const { noScroll } = useGlobal();
   const { imageOfDay: img } = useEventsOfDay();
   const [showModal, setShowModal] = useState(false);
-
-  const imgRef = useRef(null);
-  const containerRef = useRef(null);
 
   useEffect(() => {
     noScroll(showModal);
@@ -22,31 +20,12 @@ export default function ImageOfDayContainer() {
     setShowModal(false);
   };
 
-  const zoom = (e) => {
-    const realImg = imgRef.current;
-    const realWidth = realImg.naturalWidth;
-    const realHeight = realImg.naturalHeight;
-    const ratio = realHeight / realWidth;
-    const percentage = `${ratio * 100}%`;
-    const boxWidth = containerRef.current.clientWidth;
-    const xPos = e.pageX;
-    const yPos = e.pageY;
-    const xPercent = `${xPos / (boxWidth / 100)}%`;
-    const yPercent = `${yPos / ((boxWidth * ratio) / 100)}%`;
-    /* realImg.style.backgroundPosition = `${xPercent} ${yPercent}`;
-    realImg.style.backgroundSize = `${realWidth}px`; */
-    console.log(imgRef);
-    console.log({
-      percentage, boxWidth, xPos, yPos, xPercent, yPercent,
-    });
-  };
-
   const modal = showModal && (
-  <Modal onClose={handleOpenClose}>
-    <div ref={containerRef} className="modal__picture">
-      <img ref={imgRef} className="modal__img" src={img.fullImg} alt={img.alt} onMouseMove={(e) => zoom(e)} />
-    </div>
-  </Modal>
+    <Lightbox
+      mainSrc={img.fullImg}
+      onCloseRequest={handleOpenClose}
+      imageTitle={img.alt}
+    />
   );
 
   return img
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import 'react-image-lightbox/style.css';
 import App from 'App';
 
 ReactDOM.render(
